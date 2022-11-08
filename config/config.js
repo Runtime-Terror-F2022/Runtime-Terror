d6ef3d05c1f7b6b0398ef1860aa63a83d60d70f4
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-08 12:59:33 -0500

    Tournament model + mongoURI

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export const Secret = "MySecret";
 // export const MongoURI  = "mongodb://127.0.0.1/media"
-export const MongoURI  = "mongodb+srv://thiago:thiago123@mongocluster.aoylqre.mongodb.net/media1?retryWrites=true&w=majority" 
+export const MongoURI  = "mongodb+srv://tony4comp229:randompassword123@comp229-f2022-tony-3012.zvsporp.mongodb.net/projectDB?retryWrites=true&w=majority";
