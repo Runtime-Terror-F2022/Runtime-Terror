d788376c4c4b954499119446a755ceb5b7d6fa9d
salina sapkota <ssapkota8@my.centennialcollege.ca>
2022-11-08 18:35:00 -0500

    Added Functionality to Create Tournament Button

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -7,5 +7,7 @@ const router = Router();
 
 router.get('/', displayHomePage);
 router.get('/home', displayHomePage);
+router.get('/tournament-add', DisplayTournamentAddPage);
+router.post('/tournament-add', ProcessTournamentAddPage);
 
 export default router;
