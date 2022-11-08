cb4d36f5f312775533d87a7fed34c2e257691bb5
Heba Sajid <hsajid35467@gmail.com>
2022-11-08 11:33:59 -0500

    landing page created. button functionality needed

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,23 +1,23 @@
 import { UserDisplayName } from "../utils/index.js";
 
 export function displayHomePage(req, res, next) {
-    res.render('index', { title: 'Home', page: 'home', displayName: UserDisplayName(req) } );
+    res.render('index', { title: 'Host Tournaments', page: 'home', displayName: UserDisplayName(req) } );
 };
 
 export function displayAboutPage(req, res, next) {
-    res.render('index', { title: 'About', page: 'about', displayName: UserDisplayName(req)} );
+    res.render('index', { title: 'Link 1', page: 'about', displayName: UserDisplayName(req)} );
 };
 
 export function displayProjectsPage(req, res, next) {
-    res.render('index', { title: 'Projects', page: 'projects', displayName: UserDisplayName(req)} );
+    res.render('index', { title: 'Link 2', page: 'projects', displayName: UserDisplayName(req)} );
 };
 
 export function displayServicesPage(req, res, next) {
-    res.render('index', { title: 'Services', page: 'services', displayName: UserDisplayName(req)} );
+    res.render('index', { title: 'Link 3', page: 'services', displayName: UserDisplayName(req)} );
 };
 
 export function displayContactPage(req, res, next) {
-    res.render('index', { title: 'Contact', page: 'contact', displayName: UserDisplayName(req)} );
+    res.render('index', { title: 'Link 4', page: 'contact', displayName: UserDisplayName(req)} );
 };
 
 
