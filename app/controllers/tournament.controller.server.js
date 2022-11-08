6dd88e12993672a78f63c7317fd86f5f21c408ef
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-08 13:07:49 -0500

    Fully functional add tournament button on tournament list page

--- app/controllers/tournament.controller.server.js
+++ app/controllers/tournament.controller.server.js
@@ -9,3 +9,25 @@ export function DisplayTournamentList(req, res, next){
         res.render('index', {title: 'Tournament List', page: 'tournaments/list', tournaments: tournamentCollection});
     })
 }
+
+export function DisplayTournamentAddPage(req, res, next){
+    res.render('index', {title: 'Add Tournament', page: 'tournaments/edit', tournament: {}});
+}
+
+export function ProcessTournamentAddPage(req, res, next){
+    let newTournament = tournamentModel({
+        name: req.body.name,
+        game: req.body.game,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        size: req.body.size
+    });
+
+    tournamentModel.create(newTournament, (err, Tournament) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+        res.redirect('/tournament-list');
+    })
+}
--- app/routes/tournament.route.server.js
+++ app/routes/tournament.route.server.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { DisplayTournamentList} from "../controllers/tournament.controller.server.js";
+import { DisplayTournamentList, DisplayTournamentAddPage, ProcessTournamentAddPage } from "../controllers/tournament.controller.server.js";
 
 const router = Router();
 
 router.get('/tournament-list', DisplayTournamentList);
-
+router.get('/tournament-add', DisplayTournamentAddPage);
+router.post('/tournament-add', ProcessTournamentAddPage);
 
 export default router;
