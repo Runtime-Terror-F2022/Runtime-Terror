4c91bd7782f52c13c3f8c486a4249f341a8282ba
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-08 19:57:30 -0500

    Removed duplicate code

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -1,29 +1,5 @@
-import tournamentModel from '../models/tournament.js';
-
 export function displayHomePage(req, res, next) {
     res.render('index', { title: 'Host Tournaments', page: 'home' });
 };
-export function DisplayTournamentAddPage(req, res, next) {
-    res.render('index', { title: 'Add Tournament', page: 'tournaments/edit', tournament: {} });
-}
-
-export function ProcessTournamentAddPage(req, res, next) {
-    let newTournament = tournamentModel({
-        name: req.body.name,
-        game: req.body.game,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        size: req.body.size
-    });
-
-    tournamentModel.create(newTournament, (err, Tournament) => {
-        if (err) {
-            console.error(err);
-            res.end(err);
-        };
-        res.redirect('/tournament-list');
-    })
-}
-
 
 
--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
-import {
-    displayHomePage, 
-    DisplayTournamentAddPage,
-    ProcessTournamentAddPage
-    } from "../controllers/index.controller.server.js";
+import { displayHomePage } from "../controllers/index.controller.server.js";
 
 const router = Router();
 
 router.get('/', displayHomePage);
 router.get('/home', displayHomePage);
-router.get('/tournament-add', DisplayTournamentAddPage);
-router.post('/tournament-add', ProcessTournamentAddPage);
+
 
 export default router;
