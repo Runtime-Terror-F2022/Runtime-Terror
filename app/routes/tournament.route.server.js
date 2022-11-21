536f9d4f003bae242bd780c132217bf5b40e0c47
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-21 18:45:38 -0500

    Using Authguard function in tournament routes to prevent unauthenticated users from using features

--- app/routes/tournament.route.server.js
+++ app/routes/tournament.route.server.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 
 import { DisplayTournamentList, DisplayTournamentAddPage, ProcessTournamentAddPage, DisplayTournamentEditPage, ProcessTournamentEditPage, ProcessTournamentDelete, DisplayBracket } from "../controllers/tournament.controller.server.js";
 
+import { AuthGuard } from "../utils/index.js";
+
 const router = Router();
 
 //routers for displaying tournament pages.
 router.get('/tournament-list', DisplayTournamentList);
-router.get('/tournament-add', DisplayTournamentAddPage);
-router.post('/tournament-add', ProcessTournamentAddPage);
-router.get('/tournament-edit/:id', DisplayTournamentEditPage);
-router.post('/tournament-edit/:id', ProcessTournamentEditPage);
-router.get('/tournament-delete/:id', ProcessTournamentDelete);
+router.get('/tournament-add', AuthGuard, DisplayTournamentAddPage);
+router.post('/tournament-add', AuthGuard, ProcessTournamentAddPage);
+router.get('/tournament-edit/:id', AuthGuard, DisplayTournamentEditPage);
+router.post('/tournament-edit/:id', AuthGuard, ProcessTournamentEditPage);
+router.get('/tournament-delete/:id', AuthGuard, ProcessTournamentDelete);
 router.get('/tournament-view/:id', DisplayBracket);
 
 export default router;
