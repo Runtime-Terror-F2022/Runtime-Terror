afc3e4d2ce0862f7b6533283184647cba9bec2ba
Heba Sajid <hsajid35467@gmail.com>
2022-11-22 11:23:08 -0500

    code review completed

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,11 +1,14 @@
 import { Secret } from '../../config/config.js';
 
+//exporting userdisplayname function
+
 export function UserDisplayName(req){
     if(req.user){
         return req.user.displayName;
     }
     return '';
 }
+//exporting authguard function
 export function AuthGuard(req, res, next){
     if(!req.isAuthenticated()){
         return res.redirect('/login')
